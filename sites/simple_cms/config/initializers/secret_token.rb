681f9c5a338a6bad65b3f26a11c6b24a612c8350
mraybin <mark.raybin@cognitivematch.com>
2014-02-10 19:19:58 -0500

    ruby on rails simple cms update

---
+++ sites/simple_cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = 'c2fb472e9ac354da96800a13de48327a4f6d6c0fc039d79b55b8dcfeed36577e63c339876d858204388b643cd1a4ea27baef5f6f65ccb6b1ed8e51f9c8246c61'
